dcc8600434dabed9a6915834c707bd685755bfb3
phoebe61g <s111321512@mail1.ncnu.edu.tw>
2023-12-05 12:52:59 +0000

    Modify the RR format, also add zero to the last item if the filesize isn't divisible by 223.

--- fileop.py
+++ fileop.py
@@ -1,13 +1,13 @@
 def bin_split(filename):
     bin_file = open(filename, "rb")
     raw_bytes = bin_file.read()
-    n = 223# Split the message into groups
+    n = 223 # Split the message into groups
     msglist = [raw_bytes[i:i+n] for i in range(0, len(raw_bytes), n)]
     cnt = len(msglist)
     if len(raw_bytes) % n != 0:
-        num = n - len(msglist[cnt-1])
-        add = b'0' * num
-        msglist[cnt-1] = msglist[cnt-1] + add
-        print(msglist[cnt-1])
+        actual = len(msglist[cnt-1])
+        num = n - actual
+        addzero = b'0' * num
+        msglist[cnt-1] = msglist[cnt-1] + addzero
     bin_file.close()
-    return msglist
+    return msglist, actual
