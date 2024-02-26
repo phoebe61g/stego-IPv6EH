d8b4179031256c1311882c748fb1f08ee5298302
phoebe <phoebelyc.98@gmail.com>
2024-02-26 09:09:20 +0000

    Change variable name: 'redun' --> 'last'.

--- fileop.py
+++ fileop.py
@@ -1,13 +1,12 @@
 def bin_split(filename):
     bin_file = open(filename, "rb")
     raw_bytes = bin_file.read()
-    n = 223 # Split the message into groups
+    n = 223 # Split the message into 223 bytes per group.
     msglist = [raw_bytes[i:i+n] for i in range(0, len(raw_bytes), n)]
     cnt = len(msglist)
-    if len(raw_bytes) % n != 0:
-        actual = len(msglist[cnt-1])
-        num = n - actual
-        addzero = b'0' * num
+    if len(raw_bytes) % n != 0: # If the last group shorter than 223 bytes, add zero-bytes for padding.
+        last = len(msglist[cnt-1])
+        addzero = b'0' * (n - last)
         msglist[cnt-1] = msglist[cnt-1] + addzero
     bin_file.close()
-    return msglist, actual
+    return msglist, cnt, last
--- sender.py
+++ sender.py
@@ -1,40 +1,34 @@
 #!/usr/bin/python
 import sys
 import time
-import socket
 from scapy.all import *
 import getinfo
 import fileop
 import reedsolomon as rs
 codec = rs.set_codec(255, 223)
-ethertype = b"\x86\xdd" # IPv6
-clientMAC, clientIP = getinfo.src_addrs()
 dnsMAC, dnsIP = getinfo.dst_addrs()
 # Process the binary file to send
 print("Start slicing data...")
 T_slice = time.time() # Timer
 filename = sys.argv[1]
-msglist, redun = fileop.bin_split(filename)
-cw_cnt = len(msglist) # num of codewords
-enclist = []
-# RS Encoder
+msglist, cw_cnt, last = fileop.bin_split(filename)
+cwlist = []
+# RS(255,223) Encoder
 print("Start encoding...")
 T_encode = time.time()
-for i in range(cw_cnt):
-    encmsg = rs.encoder(msglist[i], codec)
-    slicing = [encmsg[i:i+16] for i in range(0, len(encmsg), 16)]
-    enclist.append(slicing)
+for i in range(cw_cnt): 
+    encmsg = rs.encoder(msglist[i], codec) # Encode 223-bytes into 255-bytes codeword.
+    cw_slicing = [encmsg[i:i+16] for i in range(0, len(encmsg), 16)]
+    cwlist.append(cw_slicing)
 # Send
-s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW)
-s.bind((str(conf.iface), 0))
 print("Start sending...")
 T_send = time.time()
 whohas_pre = filename + '.' + str(cw_cnt) + '.'
 print("--> <filename>.<codeword_cnt>.<codeword_index>.<last_bytes>.org")
 for cw in range(cw_cnt):
     for i in range(16):
-        whohas = whohas_pre + str(cw) + '.' + str(redun) + '.org'
-        pkt = IPv6(dst=dnsIP)/IPv6ExtHdrDestOpt(options=PadN(optdata=enclist[cw][i]))/UDP(sport=53, dport=53)/DNS(id=i, qd=DNSQR(qname=whohas, qtype="A"))
+        whohas = whohas_pre + str(cw) + '.' + str(last) + '.org'
+        pkt = IPv6(dst=dnsIP)/IPv6ExtHdrDestOpt(options=PadN(optdata=cwlist[cw][i]))/UDP(sport=53, dport=53)/DNS(id=i, qd=DNSQR(qname=whohas, qtype="A"))
         send(pkt, verbose=0)
         print("Packets sended: index {}".format(cw * 16 + i), end = '\r')
         #time.sleep(0.005)
@@ -44,4 +38,3 @@ print("Time for slicing: {:.2f} seconds.".format(T_encode - T_slice))
 print("Time for encoding: {:.2f} seconds.".format(T_send - T_encode))
 print("Time for sending: {:.2f} seconds.".format(T_end - T_send))
 print("Totally spent {:.2f} seconds.".format(T_end - T_slice))
-s.close()
