70df82b0d2326560063291fd6160d65c29ad290b
phoebe <phoebelyc.98@gmail.com>
2024-02-29 08:00:39 +0000

    Sender code tidy up

--- sender.py
+++ sender.py
@@ -2,8 +2,7 @@
 import sys
 import time
 from scapy.all import *
-import getinfo
-import fileop
+import getinfo, fileop
 import reedsolomon as rs
 codec = rs.set_codec(255, 223)
 dnsMAC, dnsIP = getinfo.dst_addrs()
@@ -28,7 +27,7 @@ print("--> <filename>.<codeword_cnt>.<codeword_index>.<last_bytes>.org")
 for cw in range(cw_cnt):
     for i in range(16):
         whohas = whohas_pre + str(cw) + '.' + str(last) + '.org'
-        pkt = IPv6(dst=dnsIP)/IPv6ExtHdrDestOpt(options=PadN(optdata=cwlist[cw][i]))/UDP(sport=53, dport=53)/DNS(id=i, qd=DNSQR(qname=whohas, qtype="A"))
+        pkt = IPv6(dst=dnsIP)/IPv6ExtHdrDestOpt(options=PadN(optdata=cwlist[cw][i]))/UDP(dport=53)/DNS(id=i, qd=DNSQR(qname=whohas, qtype="A"))
         send(pkt, verbose=0)
         print("Packets sended: index {}".format(cw * 16 + i), end = '\r')
         #time.sleep(0.005)
@@ -38,3 +37,4 @@ print("Time for slicing: {:.2f} seconds.".format(T_encode - T_slice))
 print("Time for encoding: {:.2f} seconds.".format(T_send - T_encode))
 print("Time for sending: {:.2f} seconds.".format(T_end - T_send))
 print("Totally spent {:.2f} seconds.".format(T_end - T_slice))
+exit()
